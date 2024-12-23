dc087d4be8279e6fd6280d720862d1c153673358
MIAS <muhammadimran.math@gmail.com>
2024-12-23 20:37:48 +0500

    callbacks included to check

--- app/src/main/java/com/miassolutions/geoquiz/MainActivity.kt
+++ app/src/main/java/com/miassolutions/geoquiz/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.miassolutions.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.miassolutions.geoquiz.databinding.ActivityMainBinding
 
+private const val TAG = "CALLBACKS_FUNCTIONS"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onStart is called")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -61,5 +64,35 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = quizViewModel.currentQuestionText
         binding.questionTextView.setText(questionTextResId)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy is called")
     }
 }
